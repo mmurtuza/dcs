3224bdd79fd956539b52eab7c3de9d0c7768f932
nasimhsagor <nasimhaidarsagor@gmail.com>
2023-08-10 15:40:11 +0600

    example test updated

--- tests/Feature/ExampleFetury test.php
+++ tests/Feature/ExampleFetury test.php
@@ -6,7 +6,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class ExampleFetury test extends TestCase
+class ExampleFetury extends TestCase
 {
     /**
      * A basic feature test example.
